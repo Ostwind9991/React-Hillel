1f9f42337a9cafaa0777b23fa92ac6ac835b3ccb
Izmar <Slonikkot1309>
2022-03-27 14:21:50 +0300

    TodoList beta

--- lesson_2/src/App.js
+++ lesson_2/src/App.js
@@ -4,11 +4,9 @@ import TodoList from './components/TodoList'
 
 
 function App() {
-  const name='Alexandr';
-  const age='27';
   return (
     <div className="App">
-      <TodoList name={name} age={age} visible={true}/>
+      <TodoList />
     </div>
   );
 }
--- lesson_2/src/components/TodoList.jsx
+++ lesson_2/src/components/TodoList.jsx
@@ -1,129 +1,99 @@
-import "./TodoList.css"
+import "./TodoList.css";
 import React, { Component } from "react";
 
 export default class TodoList extends Component {
-    state = {
-            text: "Hello",
-            isVisible:true,
-            food: [
-                {
-                  id: "14",
-                  title: "Сделать утреннюю зарядку",
-                  completed: true
-                },
-                {
-                  id: "16",
-                  title: "Купить продуктьі",
-                  completed: false
-                },
-                {
-                  id: "18",
-                  title: "Сделать домашнее задание",
-                  completed: false
-                },
-                {
-                  id: "19",
-                  title: "Сходить в кино",
-                  completed: true
-                },
-                {
-                  id: "20",
-                  title: "Позвонить другу",
-                  completed: false
-                }
-              ],
-        };
-    
-    constructor() {
-        super();
-        this.onButtonClick = this.onButtonClick.bind(this);
-    }
+  state = {
+    food: [
+      {
+        id: "14",
+        title: "Сделать утреннюю зарядку",
+        completed: true,
+      },
+      {
+        id: "16",
+        title: "Купить продуктьі",
+        completed: false,
+      },
+      {
+        id: "18",
+        title: "Сделать домашнее задание",
+        completed: false,
+      },
+      {
+        id: "19",
+        title: "Сходить в кино",
+        completed: true,
+      },
+      {
+        id: "20",
+        title: "Позвонить другу",
+        completed: false,
+      },
+    ],
+  };
 
-    render(){
-        const {isVisible, text, food} = this.state;
-        return (
-        <>
-        <input type="text" onChange={this.onInputChange.bind(this)}/>
-        <button onClick={this.onInputAdd.bind(this)}>Add</button>
-        {isVisible && <h1>{text}</h1>}
-        <button onClick={this.onButtonClick}>
-            {isVisible ? 'Hide' :'Show'}
-        </button>
+  constructor() {
+    super();
+    this.onInputAdd = this.onInputAdd.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  render() {
+    const { food } = this.state;
+    return (
+      <>
+        <input type="text" onChange={this.onInputChange} />
+        <button onClick={this.onInputAdd}>Add</button>
+        <h1>TodoList</h1>
         <ul>
-            {food.map(el=> <li key={el.id}>{el.title}
-            <button className={el.completed ?"DiActive" : "Active"} key={el.id+"1"}  onClick={() => this.onInputDel(el)}>Delite</button></li>)}
-            {console.log(this.state.food)}
+          {food.map((el) => (
+            <li key={el.id} className={el.completed ? "DiActive" : "Active"}>
+              <input
+                type="checkbox"
+                defaultChecked={el.completed ? true : false}
+                onChange={() => this.onCheckedChange(el)}
+              />
+              {el.title}
+              <button key={el.id + "1"} onClick={() => this.onInputDel(el)}>
+                Delite
+              </button>
+            </li>
+          ))}
         </ul>
-        
-        </>
-        );
-    }
+      </>
+    );
+  }
 
-    onInputChange(e){
-        console.log(e.target.value)
-        this.setState({newFood: e.target.value})
-    }
+  onInputChange(e) {
+    const { newFood } = this.state;
+    this.setState({ newFood: e.target.value });
+    console.log(newFood);
+  }
 
-    onInputAdd(){
-        const arrFood = {};
-        const index = parseInt(this.state.food[this.state.food.length - 1].id)
-        console.log(index+1)
-        arrFood.id= `${index+1}`;
-        arrFood.title=`${this.state.newFood}`;
-        arrFood.completed=false;
-        this.state.food.push(arrFood);
-        this.setState(this.state.food)
-    }
+  onCheckedChange(el) {
+    const { food } = this.state;
+    food.find((elem) => elem.id === el.id).completed = !food.find(
+      (elem) => elem.id === el.id
+    ).completed;
+    this.setState(food);
+  }
 
-    onInputDel(el){
-        this.state.food.find(elem => elem.id === el.id).completed = !this.state.food.find(elem => elem.id === el.id).completed
-        this.setState(this.state.food)
+  onInputAdd() {
+    const { food, newFood } = this.state;
+    const arrFood = {};
+    const index = parseInt(food[food.length - 1].id);
+    console.log(index + 1);
+    arrFood.id = `${index + 1}`;
+    arrFood.title = `${newFood}`;
+    arrFood.completed = false;
+    food.push(arrFood);
+    this.setState(food);
+  }
 
-    }
-
-    onButtonClick() {
-        const {isVisible} = this.state;
-        this.setState({isVisible: !isVisible})
-    }
+  onInputDel(el) {
+    const { food } = this.state;
+    const index = food.findIndex((x) => x.id === el.id);
+    food.splice(index, 1);
+    this.setState({ food: food });
+  }
 }
-
-
-
-
-
-
-
-// import React, { Component } from "react";
-
-// export default class TodoList extends Component {
-//     state = {
-//             text: "Hello",
-//             isVisible:true,
-//             food: ["eggs", "bread", "milk"],
-//         };
-    
-//     constructor() {
-//         super();
-//         this.onButtonClick = this.onButtonClick.bind(this);
-//     }
-
-//     render(){
-//         const {isVisible, text, food} = this.state;
-//         return (
-//         <>
-//         {isVisible && <h1>{text}</h1>}
-//         <button onClick={this.onButtonClick}>
-//             {isVisible ? 'Hide' :'Show'}
-//         </button>
-//         <ul>
-//             {food.map(el=> <li key={el}>{el}</li>)}
-//         </ul>
-//         </>
-//         );
-//     }
-
-//     onButtonClick() {
-//         const {isVisible} = this.state;
-//         this.setState({isVisible: !isVisible})
-//     }
-// }
