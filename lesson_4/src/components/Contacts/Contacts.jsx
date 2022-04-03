7f31c931d2f5ec7d317bca871de6345f4588534c
Izmar <Slonikkot1309>
2022-04-04 02:57:14 +0300

    lessons_4 v0.2

--- lesson_4/src/components/Contacts/Contacts.jsx
+++ lesson_4/src/components/Contacts/Contacts.jsx
@@ -1,14 +1,13 @@
 import "./Contacts.css";
 
-
 export default function Contacts(props) {
   const { name, phone, id, sername } = props.contact;
-  return (    
+  return (
     <li className="contact">
-    <span>{name}</span>
-    <span>{sername}</span>
-    <span>{phone}</span>
-    <button onClick={() => props.onClickDelete(id)}>Delete</button>
-  </li>
+      <span>{name}</span>
+      <span>{sername}</span>
+      <span>{phone}</span>
+      <button onClick={() => props.onClickDelete(id)}>Delete</button>
+    </li>
   );
 }
--- lesson_4/src/components/Forma/Forma.jsx
+++ lesson_4/src/components/Forma/Forma.jsx
@@ -1,68 +1,52 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./Forma.css";
 
-export default class Forma extends Component {
+export default function Forma(props) {
+  const [name, setName] = useState("");
+  const [sername, setSername] = useState("");
+  const [phone, setPhone] = useState("");
+  const onSubmit = () => {
+    const newElem = {
+      name: name,
+      phone: phone,
+      sername: sername,
+    };
+    props.onSubmitButton(newElem);
+  };
 
-
-  constructor() {
-    super();
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  render() {
-    return (
-      <>
-              <div className={"wrapper-forma " }>
+  return (
+    <>
+      <div className={"wrapper-forma "}>
         <form className="forma">
           <h2>Добавить контакт</h2>
           <input
             type="text"
             name="name"
-            onChange={this.onInputChange}
+            onChange={(event) => setName(event.target.value)}
             placeholder="Имя"
           />
           <input
             type="text"
             name="sername"
-            onChange={this.onInputChange}
+            onChange={(event) => setSername(event.target.value)}
             placeholder="Фамилия"
           />
           <input
             type="phone"
             name="phone"
-            onChange={this.onInputChange}
+            onChange={(event) => setPhone(event.target.value)}
             placeholder="Телефон"
           />
           <div>
-            <button className="add" onClick={this.onSubmit}>
+            <button className="add" onClick={onSubmit}>
               Сохранить
             </button>
-            <button className="close" onClick={this.props.onShowContact}>
+            <button className="close" onClick={props.onShowContact}>
               Отмена
             </button>
           </div>
         </form>
-        </div>
-      </>
-    );
-  }
-  onInputChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  onSubmit(e){
-    e.preventDefault();
-    const newElem = {
-      name: this.state.name,
-      phone: this.state.phone,
-      sername: this.state.sername,
-    };
-    this.props.onSubmitButton(newElem);
-
-  }
-
-
+      </div>
+    </>
+  );
 }
