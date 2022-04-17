e43d839524c51d6a16bd630c0b9fc04b32d0b068
Izmar <Slonikkot1309>
2022-04-17 22:35:01 +0300

    lesson_6 v 0.1

--- lesson_6/src/services/contactsService.js
+++ lesson_6/src/services/contactsService.js
@@ -1,27 +1,17 @@
-const URL = "https://612687da3ab4100017a68fd8.mockapi.io/contacts/";
+import api from "../api/api";
 
 export function createContact(contact) {
-  return fetch(URL, {
-    method: "POST",
-    body: JSON.stringify(contact),
-    headers: { "Content-Type": "application/json" },
-  }).then((res) => res.json());
+  return api.post("", contact).then((res) => res.data);
 }
 
 export function updateContact(contact) {
-  return fetch(URL + "/" + contact.id, {
-    method: "PUT",
-    body: JSON.stringify(contact),
-    headers: { "Content-Type": "application/json" },
-  }).then((res) => res.json());
+  return api.put("" + "/" + contact.id).then((resp) => resp.data);
 }
 
 export function updateContactTable() {
-  return fetch(URL).then((resp) => resp.json());
+  return api.get().then((resp) => resp.data);
 }
 
 export function deleteContact(id) {
-  return fetch(URL + id, {
-    method: "DELETE",
-  }).then((res) => res.json());
+  return api.delete("" + id).then((resp) => resp.data);
 }
